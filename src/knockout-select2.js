3231c8b8b8da34ee0196d6d744d0a809e41ee5ec
Robert DeSimone <icenine457@gmail.com>
2016-07-13 06:56:26 -0400

    Fix check for webpacked file

--- src/knockout-select2.js
+++ src/knockout-select2.js
@@ -9,7 +9,7 @@
     'use strict';
 
     var bindingName = 'select2';
-    if (module && module.config() && module.config().name) {
+    if (module && typeof(module.config) === 'function' && module.config() && module.config().name) {
         bindingName = module.config().name;
     }
 
